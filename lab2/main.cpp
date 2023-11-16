2372ac0f6a148d426cf29cbf0ba2eea497e172b8
wednees <postnov.dart-weyder@yandex.ru>
2023-11-16 15:38:50 +0300

    cleaning

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,40 +1,8 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <pthread.h>
-// #include <time.h>
-// #include <sys/time.h>
-// #include <math.h>
-
-// typedef struct ThreadData {
-//     int in_points;
-//     int total_points;
-//     double radius;
-// } Spot;
-
-// void* monteCarlo(void* arg) {
-//     struct ThreadData* th_data = (struct ThreadData*)arg;
-//     double radius = th_data->radius; 
-//     unsigned int seed = (unsigned int)time(NULL);
-
-//     for(int i = 0; i < th_data->total_points; ++i) {
-//         double x = (((double)rand_r(&seed) / RAND_MAX) * 2 * radius) - radius;
-//         double y = (((double)rand_r(&seed) / RAND_MAX) * 2 * radius) - radius;
-
-
-//         if(x * x + y * y <= radius * radius) {
-//             th_data->in_points++;
-//         }
-//     }
-    
-//     pthread_exit(NULL);
-// }
-
 #include "include.hpp"
 
 int main(int argc, char* argv[]) {
     if(argc != 4) {
         cout << "Usage: " << argv[0] << " <radius> <number of threads> <number of points (increses accuracy)>" << endl;
-        // printf("Usage: %s <radius> <number of th>\n", argv[0]);
         exit (-1);
     }
 
@@ -75,20 +43,16 @@ int main(int argc, char* argv[]) {
         points_inside_circle += th_data[i].in_points;
     }
    
-   // printf("%d   ,   %.6lf\n", points_inside_circle, (double)points_inside_circle / points);
     double rel = (double) points_inside_circle / (double) points;
     double area_of_square = 4 * radius * radius;
-    // cout << fixed << setprecision(3) << rel << " " << area_of_square << endl;
     double area_of_circle = area_of_square * rel ;
 
     cout << fixed << setprecision(3) << "Area of the circle: " << area_of_circle << "; Radius: " << radius << endl;
-    // printf("Area of the cirlce with radius %.3lf: %.3lf \n", radius, area_of_circle);
 
     seconds = end.tv_sec - start.tv_sec;
     micro_seconds = end.tv_usec - start.tv_usec;
     double time = seconds + micro_seconds;
     cout << fixed << setprecision(3) << "Execution time: " << time << "ms" << endl;
-    // printf("Execution time: %.3f ms\n", time);
 
     delete(th);
     delete(th_data); 
--- lab2/src.cpp
+++ lab2/src.cpp
@@ -9,8 +9,7 @@
 void* monte_carlo(void* arg) {
     struct ThreadData* data = (struct ThreadData*)arg;
     double radius = data->radius; 
-    // unsigned int seed = (unsigned int)time(NULL);
-    //cout << "radius: " << radius << endl;
+
     for(int i = 0; i < data->total_points; ++i) {
         
         double min = -1 * radius;
@@ -24,15 +23,10 @@ void* monte_carlo(void* arg) {
         
         y = rand() % (int)pow(10, precision);
         y = min + (y / pow(10, precision)) * (max - min);
-        // double x = (((double)rand_r(&seed) / RAND_MAX) * 2 * radius) - radius;
-        // double y = (((double)rand_r(&seed) / RAND_MAX) * 2 * radius) - radius;
-
-        // cout << "x: " << x << "y: " << y << endl;
 
         if(x * x + y * y <= radius * radius) {
             data->in_points++;
         }
     }
-    // cout << data->total_points << " " << data->in_points << endl;
     pthread_exit(NULL);
 }
